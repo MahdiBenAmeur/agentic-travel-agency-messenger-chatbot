2beb8a3b36289359fa38bb5d3ef6f31dfd620b07
mahdiBA <mahdi.benameur.98@gmail.com>
2025-12-17 20:09:24 +0100

    accept all incomming requests

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.trip import router as trips_router
 from routes.client import router as clients_router
 from routes.booking import router as bookings_router
 from routes.message import router as messages_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],      
+    allow_credentials=True,
+    allow_methods=["*"],      
+    allow_headers=["*"],      
+)
+
 app.include_router(trips_router)
 app.include_router(clients_router)
 app.include_router(bookings_router)
 app.include_router(messages_router)
-
 
 if __name__ == "__main__":
     import uvicorn
